592aef4187a7726022f6778d1569fac923da7362
StevenGandon <steven.gandon@epitech.eu>
2024-04-15 16:03:27 +0200

    [ADD] unit test for compiler file creation

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,5 +1,7 @@
 from test_common import *
 
+from genericpath import isfile
+
 @tests(name="test_compiler_no_argv")
 def test() -> bool:
     return (exec_shell('../compiler/fcc')[0] == 84)
@@ -23,4 +25,18 @@ def test() -> bool:
 @tests(name="test_compiler_invalid_multiple_argv")
 def test() -> bool:
     return (exec_shell('../compiler/fcc assets/test0.fly ezfzezef assets')[0] == 84)
+
+@tests(name="test_compiler_valid_argv_create_binary")
+def test() -> bool:
+    exec_shell('../compiler/fcc assets/test0.fly')
+    if (not isfile("assets/test0.flo")):
+        return (False)
+    return (True)
+
+@tests(name="test_compiler_invalid_argv_create_binary")
+def test() -> bool:
+    exec_shell('../compiler/fcc assets/invalid_file.fly')
+    if (isfile("assets/invalid_file.flo")):
+        return (False)
+    return (True)
 
