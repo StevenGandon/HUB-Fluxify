4a369e79b07ffb361db6dce9c5b74720cd59236e
Fizo55 <hugomacees56000@gmail.com>
2024-05-02 16:43:18 +0200

    [UPDATE] Start rework using floff

--- vm/lib/fluxify/main.c
+++ vm/lib/fluxify/main.c
@@ -5,22 +5,34 @@
 ** main.c
 */
 
-#include "fluxify.h"
+#include "floff.h"
+#include <string.h>
 #include <stdio.h>
+
+static void load_x64_program(const char *filename)
+{
+    floff64_t floff64;
+    int result = 0;
+
+    memset(&floff64, 0, sizeof(floff64));
+    result = read_floff64(&floff64, filename);
+    if (result == -1) {
+        fprintf(stderr, "Failed to read .flo file: %s\n", filename);
+        return;
+    }
+    // TODO
+    // program = convert_floff64_to_program(&floff64);
+    // if (program == NULL) {
+    //     fprintf(stderr, "Failed to convert .flo data to program structure\n");
+    //     return NULL;
+    // }
+    destroy_floff64(&floff64);
+}
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename.flo>\n", argv[0]);
-        return 84;
-    }
-
-    program_t *program = load_program(argv[1]);
-
-    if (!program)
-        return 84;
-    run_program(program);
-    gc_collect(program);
-    free_program(program);
+    (void)argc;
+    (void)argv;
+    load_x64_program(argv[1]);
     return 0;
 }
