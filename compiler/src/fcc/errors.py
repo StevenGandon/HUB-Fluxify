6ff490820927c6c5b4ff0c5ff2310a19a02a9cc7
dkeenan <keenan.deboissy@epitech.eu>
2024-05-02 17:38:22 +0200

    [ADD] Invalid assign error

--- compiler/src/fcc/errors.py
+++ compiler/src/fcc/errors.py
@@ -180,3 +180,31 @@ def invalid_for_condition(code: object, debug_constructor: object, color: str, p
                         ' ' * (len(item) - len(item.lstrip())) + color + '^' + '~' * (len(item.lstrip().rstrip()) - 1) + RESET_COLOR
                     ]
                 ))
+
+def invalid_assign_name(code: object, debug_constructor: object, color: str, prefix: str = 'E') -> None:
+    in_comment: bool = False
+
+    for i, item in enumerate(code.code.replace('\\\n', '').split("\n")):
+        if (bool(search(REGEX_MULTILINE_COMMENT_PREFIX, item))):
+            in_comment: bool = True
+        if ("<==" in item):
+            in_comment: bool = False
+        item = item.split('=>')[0]
+        token = code.get_token(item)
+
+        if (in_comment or not isinstance(token, AssignToken)):
+            continue
+
+        if (not token.name):
+            code.debug.append(
+                debug_constructor(
+                    code.filename,
+                    (len(item) - len(item.lstrip())) + 1,
+                    i + 1,
+                    f"{prefix}invalid_assign_name",
+                    f"Invalid assign name.",
+                    display=[
+                        color + item + RESET_COLOR,
+                        ' ' * (len(item) - len(item.lstrip())) + color + '^' + '~' * (len(item.lstrip().rstrip()) - 1) + RESET_COLOR
+                    ]
+                ))
