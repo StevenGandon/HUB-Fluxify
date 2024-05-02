738976d2aff7fffd20d86c4868957cc8b97c91bd
Fizo55 <hugomacees56000@gmail.com>
2024-05-02 17:56:26 +0200

    [FIX] Vm launch

--- vm/include/fluxify.h
+++ vm/include/fluxify.h
@@ -29,62 +29,9 @@
         _type_object_t *object_type;
     };
 
-    typedef struct header64_s {
-        uint32_t magic;
-        uint16_t format_version;
-        uint16_t architecture;
-        uint8_t compiler_name_size;
-        char *compiler_name;
-        uint8_t program_hash[40];
-        uint64_t table_number;
-        uint64_t start_label_address;
-    } header64_t;
-
-    typedef enum {
-        NOOP = 0, ADD, SUB
-    } InstructionCode;
-
-    typedef struct instruction_s {
-        InstructionCode code;
-        int args[2];
-    } instructions_t;
-
-    typedef struct label_s {
-        char *label;
-        uint64_t address; // uint32_t for 32 bits ?
-    } label_t;
-
-    typedef struct constant_s {
-        int type;
-        union {
-            int i;
-            char *s;
-        } value;
-    } constant_t;
-
-    typedef struct program_s {
-        struct instruction_s *instructions;
-        size_t num_instructions;
-        struct label_s *labels;
-        size_t num_labels;
-        struct constant_s *constants;
-        size_t num_constants;
-        FlObject **objects;
-        size_t num_objects;
-        size_t capacity_objects;
-    } program_t;
-
-    FlObject *create_object(_type_object_t *type);
-    void delete_object(FlObject *obj);
-    void free_object(FlObject *obj);
-
-    void incref(FlObject *obj);
-    void decref(FlObject *obj);
-
-    program_t *load_program(const char *filename);
-    void run_program(program_t *program);
-    void free_program(program_t *program);
-
-    void gc_collect(program_t *program);
+    typedef struct config_s {
+        const char *filename;
+        unsigned short arch;
+    } config_t;
 
 #endif /* FLUXIFY_H_ */
--- vm/main.c
+++ vm/main.c
@@ -5,29 +5,66 @@
 ** main.c
 */
 
-#include "fluxify.h"
 #include "floff.h"
-#include "main.h"
+#include "fluxify.h"
+#include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
 
-int
-main(argc, argv)
-    int argc;
-    char **argv;
+int parse_arguments(int argc, char **argv, config_t *config)
 {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename.flo>\n", argv[0]);
-        return 84;
+    int opt;
+    config->arch = ARCH_X64_32;
+
+    while ((opt = getopt(argc, argv, "a:")) != -1) {
+        switch (opt) {
+            case 'a':
+                if (strcmp(optarg, "X86_64") == 0) {
+                    config->arch = ARCH_X86_64;
+                } else if (strcmp(optarg, "X64_32") == 0) {
+                    config->arch = ARCH_X64_32;
+                } else {
+                    fprintf(stderr, "Unsupported architecture: %s\n", optarg);
+                    return -1;
+                }
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-a ARCH] <filename>\n", argv[0]);
+                return -1;
+        }
     }
 
-    program_t *program = load_program(argv[1]);
+    if (optind < argc) {
+        config->filename = argv[optind];
+    } else {
+        fprintf(stderr, "Missing filename.\n");
+        return -1;
+    }
+    return 0;
+}
 
-    if (!program)
+static void load_program(const char *filename)
+{
+    void *result = auto_floff(filename);
+
+    if (result == NULL) {
+        fprintf(stderr, "Corruped .flo file: %s\n", filename);
+        return;
+    }
+    // TODO
+    // program = convert_floff64_to_program(&floff64);
+    // if (program == NULL) {
+    //     fprintf(stderr, "Failed to convert .flo data to program structure\n");
+    //     return NULL;
+    // }
+}
+
+int main(int argc, char **argv)
+{
+    config_t config;
+    if (parse_arguments(argc, argv, &config) != 0)
         return 84;
-    run_program(program);
-    gc_collect(program);
-    free_program(program);
+
+    load_program(config.filename);
     return 0;
 }
