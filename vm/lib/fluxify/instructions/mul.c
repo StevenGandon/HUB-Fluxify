09e79597b5a91c714980a87e64cd9ff2c328a651
Fizo55 <hugomacees56000@gmail.com>
2024-05-16 12:46:40 +0200

    [ADD] mul instruction

--- vm/lib/fluxify/instructions/mul.c
+++ vm/lib/fluxify/instructions/mul.c
@@ -2,19 +2,19 @@
 ** EPITECH PROJECT, 2024
 ** Hub project
 ** File description:
-** sub
+** mul
 */
 
 #include "fluxify.h"
 #include <stdio.h>
 
-void fun_sub(vm_state_t *vm)
+void fun_mul(vm_state_t *vm)
 {
     instruction_t inst;
 
     inst.opcode = vm->memory[vm->program_counter];
     inst.operands[0] = vm->memory[vm->program_counter + 1];
     inst.operands[1] = vm->memory[vm->program_counter + 2];
-    vm->registers[0] = inst.operands[0] - inst.operands[1];
-    printf("SUB: %d - %d = %d\n", inst.operands[0], inst.operands[1], vm->registers[0]);
+    vm->registers[0] = inst.operands[0] * inst.operands[1];
+    printf("MUL: %d * %d = %d\n", inst.operands[0], inst.operands[1], vm->registers[0]);
 }
