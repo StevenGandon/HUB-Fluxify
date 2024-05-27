a7cc9561ea42435dc9a454a3d1d3003f1db9f451
StevenGandon <steven.gandon@epitech.eu>
2024-05-27 13:12:25 +0200

    [ADD] store inputs

--- fli/src/replaced_input.py
+++ fli/src/replaced_input.py
@@ -1,5 +1,7 @@
 from .getch import *
 
+CACHED_INPUTS = []
+
 def _str_to_ascii(text: str) -> int:
     return (int.from_bytes(text, 'little'))
 
@@ -7,6 +9,7 @@ def custom_input(display: str = '') -> str:
     pressed: bytes = b'0'
     cursor = 0
     text: str = ''
+    y_cursor = len(CACHED_INPUTS)
 
     print(display, end='', flush=True)
     while (_str_to_ascii(pressed) not in (13, 10)):
@@ -41,6 +44,18 @@ def custom_input(display: str = '') -> str:
             if (temp[0] in (ord('M'), ord('C')) and cursor + 1 <= len(text)):
                 print(text[cursor], end='', flush=True)
                 cursor += 1
+            if (temp[0] in (ord('P'), ord('B')) and y_cursor < len(CACHED_INPUTS) - 1):
+                print('\r' + ((len(text) + 1 + len(display)) * ' '), end='', flush=True)
+                y_cursor += 1
+                text = CACHED_INPUTS[y_cursor]
+                cursor = len(text)
+                print('\r' + display + text + '\b' * (len(text) - cursor), end='', flush=True)
+            if (temp[0] in (ord('H'), ord('A')) and y_cursor > 0):
+                print('\r' + ((len(text) + 1 + len(display)) * ' '), end='', flush=True)
+                y_cursor -= 1
+                text = CACHED_INPUTS[y_cursor]
+                cursor = len(text)
+                print('\r' + display + text + '\b' * (len(text) - cursor), end='', flush=True)
         elif (_str_to_ascii(pressed) in (13, 10)):
             continue
         elif (_str_to_ascii(pressed) in (0, 3)):
@@ -55,6 +70,9 @@ def custom_input(display: str = '') -> str:
             cursor += 1
             print('\r' + display + text + '\b' * (len(text) - cursor), end='', flush=True)
     print('\n', end = '')
+
+    if (not CACHED_INPUTS or CACHED_INPUTS[-1] != text):
+        CACHED_INPUTS.append(text)
 
     return (text)
 
