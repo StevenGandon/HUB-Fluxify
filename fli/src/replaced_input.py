cf38b4d8c5188f151c70c8a246b9a2557d7a44ac
StevenGandon <steven.gandon@epitech.eu>
2024-05-27 12:16:51 +0200

    [FIX] input bar

--- fli/src/replaced_input.py
+++ fli/src/replaced_input.py
@@ -22,9 +22,9 @@ def custom_input(display: str = '') -> str:
             return '\x03'
 
         if (_str_to_ascii(pressed) in (8, 127)):
-            text = text[:-1]
-            print("\b \b", end='', flush=True)
+            text = text[:cursor - 1] + text[cursor:]
             cursor -= 1
+            print(len(text) * ' ' + '\r' + display + text + '\b' * (len(text) - cursor), end='', flush=True)
         elif (_str_to_ascii(pressed) in (0xe0, 0x1b)):
             temp = getch()
             if (temp == b'['):
@@ -32,7 +32,7 @@ def custom_input(display: str = '') -> str:
             if (temp[0] in (ord('K'), ord('D')) and cursor - 1 >= 0):
                 print('\b', end='', flush=True)
                 cursor -= 1
-            if (temp[0] in (ord('M'), ord('B')) and cursor + 1 <= len(text)):
+            if (temp[0] in (ord('M'), ord('C')) and cursor + 1 <= len(text)):
                 print(text[cursor], end='', flush=True)
                 cursor += 1
         elif (_str_to_ascii(pressed) in (13, 10)):
