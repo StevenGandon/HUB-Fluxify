701d0a30359a73630683331a437afeda220d07e5
StevenGandon <steven.gandon@epitech.eu>
2024-05-31 17:40:20 +0200

    [ADD] 64 bits patterns

--- compiler/src/fcc/patterns.py
+++ compiler/src/fcc/patterns.py
@@ -41,6 +41,185 @@ class CodeStackGeneration(object):
 class Pattern64(object):
     _size = 8
 
+class PatternAdd64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x01" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternSubstract64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x02" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternMul64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x03" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternDiv64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x04" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternMod64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x05" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternEqual64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x09" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternAnd64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x0a" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternOr64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x0b" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternBinAnd64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x06" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternBinOr64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x07" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternBinXor64(Pattern64):
+    def __init__(self, fetch_num = 0) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x08" + self.fetch_num.to_bytes(self.__class__._size, "big"))
+
+class PatternAlloc64(Pattern64):
+    _area_stack_start = 0x0A
+    _area_stack_res = []
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.ptr = self.__class__._area_stack_start + self._find_empty_addr()
+
+        self.__class__._area_stack_res.append(self.ptr - self.__class__._area_stack_start)
+
+    def _find_empty_addr(self) -> None:
+        i = 0
+
+        while i in self.__class__._area_stack_res:
+            i += 1
+        return (i)
+
+    def to_code(self) -> bytes:
+        return (b"\x43" + self.ptr.to_bytes(self.__class__._size, "big"))
+
+class PatternFree64(Pattern64):
+    def __init__(self, addr) -> None:
+        super().__init__()
+
+        self.addr = addr
+
+        PatternAlloc32._area_stack_res.pop(PatternAlloc32._area_stack_res.index(addr - PatternAlloc32._area_stack_start))
+
+    def to_code(self) -> bytes:
+        return (b"\x44" + self.addr.to_bytes(self.__class__._size, "big"))
+
+class PatternFetchBlcks64(Pattern64):
+    def __init__(self, addr: int, fetch_num: int) -> None:
+        super().__init__()
+
+        self.addr = addr
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return (b"\x45" + self.fetch_num.to_bytes(self.__class__._size, "big") + self.addr.to_bytes(self.__class__._size, "big"))
+
+class PatternStoreFetch64(Pattern64):
+    def __init__(self, addr: int, fetch_num: int) -> None:
+        super().__init__()
+
+        self.addr = addr
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return b"\x46" + self.fetch_num.to_bytes(self.__class__._size, "big") + self.addr.to_bytes(self.__class__._size, "big")
+
+class PatternFetchConst64(Pattern64):
+    def __init__(self, addr: int, fetch_num: int) -> None:
+        super().__init__()
+
+        self.addr = addr
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return b"\x47" + self.fetch_num.to_bytes(self.__class__._size, "big") + self.addr.to_bytes(self.__class__._size, "big")
+
+class PatternPcFetch64(Pattern64):
+    def __init__(self, fetch_num: int) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return b"\x49" + self.fetch_num.to_bytes(self.__class__._size, "big")
+
+class PatternFetchPc64(Pattern64):
+    def __init__(self, fetch_num: int) -> None:
+        super().__init__()
+
+        self.fetch_num = fetch_num
+
+    def to_code(self) -> bytes:
+        return b"\x50" + self.fetch_num.to_bytes(self.__class__._size, "big")
+
 class Pattern32(Pattern64):
     _size = 4
 
