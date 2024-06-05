e9f80777525b2400f4975e8d2fad852be801b2b5
StevenGandon <steven.gandon@epitech.eu>
2024-06-05 12:03:28 +0200

    [ADD] base for the linker

--- compiler/src/fcc/linker.py
+++ compiler/src/fcc/linker.py
@@ -1,3 +1,19 @@
+from os.path import isfile
+from src.floff import *
+
 class Linker(object):
-    def __init__(self, files: list) -> None:
-        pass
+    def __init__(self, files: list, arch: int = ARCH_X86_64) -> None:
+        self.arch = arch
+        self.files = files if all(isfile(file) for file in files) else []
+
+    def link(self) -> FloffAuto:
+        result = Floff64() if (self.arch == ARCH_X86_64) else Floff32()
+
+        for i, file in enumerate(self.files):
+            floff_file: Floff64 = FloffAuto(file)
+
+            if (i == 0):
+                for item in floff_file.tables:
+                    result.add_table(item)
+
+        result.code_hash = "0000000000000000000000000000000000000000"
